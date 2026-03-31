480d730812248894192a4ac97253172d1fda7126
s236603 <s236603@NBN205E024.hsu.edu.hk>
2026-03-31 10:04:36 +0800

    Ver3.0

--- src/ClassA.java
+++ src/ClassA.java
@@ -12,6 +12,7 @@ public class ClassA {
         
 		System.out.println("Hello from Class B!"); 
                 System.out.println("1.2");
+                System.out.println("3.0");
               
 
     }
