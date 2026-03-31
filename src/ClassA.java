481ffe3947d4173aaa89b36ce12c33c3d5b22730
s236603 <s236603@NBN205E024.hsu.edu.hk>
2026-03-31 09:41:33 +0800

    Ver1

---
+++ src/ClassA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Chan
+ */
+public class ClassA {
+    public static void printMessage() {
+        
+		System.out.println("Hello from Class B!"); 
+                System.out.println("intitial");
+
+    }
+    
+}
---
+++ src/ClassB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Chan
+ */
+public class ClassB {
+    public static void printMessage() {
+		
+        System.out.println("Hello from Class B!");
+        
+    }
+    
+}
